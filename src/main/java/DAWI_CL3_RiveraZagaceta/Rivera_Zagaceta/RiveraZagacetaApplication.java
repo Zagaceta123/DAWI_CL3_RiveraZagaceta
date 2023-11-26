5736dc1fcdb03d53a7cbf3291345c930427c4aa9
Enzo008 <enzoaguirre629@gmail.com>
2023-11-26 14:40:17 -0500

    Inicio

---
+++ src/main/java/DAWI_CL3_RiveraZagaceta/Rivera_Zagaceta/RiveraZagacetaApplication.java
@@ -0,0 +1,13 @@
+package DAWI_CL3_RiveraZagaceta.Rivera_Zagaceta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiveraZagacetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiveraZagacetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DAWI_CL3_RiveraZagaceta/Rivera_Zagaceta/RiveraZagacetaApplicationTests.java
@@ -0,0 +1,13 @@
+package DAWI_CL3_RiveraZagaceta.Rivera_Zagaceta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiveraZagacetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
